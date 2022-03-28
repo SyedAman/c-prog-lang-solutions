346599f206ccc8f0476916fd7bbc9a61afd40c33
Syed Aman <syednashikaman@gmail.com>
2022-03-27 22:53:29 -0400

    Separate out declarations to header files.

--- celsius.h
+++ celsius.h
@@ -1,29 +1,3 @@
-#include <stdio.h>
-
-int fromFahr(int fahrenheit)
-{
-	return 5 * (fahrenheit - 32) / 9;
-}
-
-void printFahrenheitRange(int lower, int upper, int skip)
-{
-	int fahrenheit = lower;
-	int celsius;
-
-	while (fahrenheit <= upper)
-	{
-		celsius = fromFahr(fahrenheit);
-		printf("%3d\t%6d\n", fahrenheit, celsius);
-		fahrenheit += skip;
-	}
-}
-
-void promptConvertFahr()
-{
-	int inFahr;
-        printf("What degrees Fahrenheit do you want to convert to Celsius?: ");
-        scanf("%d", &inFahr);
-        int outCelsius = fromFahr(inFahr);
-        printf("%dF in Celsius is %dC.\n", inFahr, outCelsius);
-}
-
+int fromFahr(int fahrenheit);
+void printFahrenheitRange(int upper, int lower, int skip);
+void promptConvertFahr();
